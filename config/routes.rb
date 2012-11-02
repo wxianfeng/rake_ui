616e8c58217eb71c074f8686fbaa9f3634ed0dfb
wxianfeng <wxianfeng@wxianfeng.(none)>
2012-11-02 20:23:13 +0800

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 RakeUi::Engine.routes.draw do
   root :to  => "rake_tasks#index"
+  mount_at = RakeUi::Engine.config.mount_at
+
+  match mount_at => 'rake_ui/rake_tasks#index'
+
   resources :rake_tasks
 end
